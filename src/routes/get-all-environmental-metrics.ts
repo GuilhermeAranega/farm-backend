26c465e303b42827186d5900b2048845703baa4e
Guilherme Aranega <guilhermee.aranega@gmail.com>
2025-04-07 18:06:35 -0300

    refactor: rename routes

---
+++ src/routes/get-all-environmental-metrics.ts
@@ -0,0 +1,52 @@
+import { prisma } from "../lib/prisma";
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { subDays } from "date-fns";
+
+export async function getAllEnvironmentalMetrics(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    "/environmentalmetrics",
+    {
+      schema: {
+        response: {
+          200: z.object({
+            message: z.string(),
+            status: z.boolean(),
+
+            environmentalData: z.array(
+              z.object({
+                soilMoisture: z.number(),
+                airTemperature: z.number(),
+                airHumidity: z.number(),
+                lightIntensity: z.number(),
+
+                avgAirHumidity: z.number().nullable(),
+                avgAirTemp: z.number().nullable(),
+                avgLightIntensity: z.number().nullable(),
+                avgSoilMoisture: z.number().nullable(),
+                totalEntries: z.number().nullable(),
+
+                timestamp: z.date(),
+              })
+            ),
+          }),
+        },
+      },
+    },
+    async (req, res) => {
+      const sevenDaysAgo = subDays(new Date(), 7);
+
+      const environmentalData = await prisma.environmentalData.findMany({
+        where: { timestamp: { gte: sevenDaysAgo } },
+        orderBy: { timestamp: "desc" },
+      });
+
+      return res.status(200).send({
+        message: "Found environmental data",
+        status: true,
+        environmentalData,
+      });
+    }
+  );
+}
