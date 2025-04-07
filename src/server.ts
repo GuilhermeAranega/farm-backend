8c2c0e310f3825a6afe5af62c7807ccabf070ac0
Guilherme Aranega <guilhermee.aranega@gmail.com>
2025-04-07 18:09:26 -0300

    refactor: rename the imports

--- src/server.ts
+++ src/server.ts
@@ -8,8 +8,8 @@ import {
 } from "fastify-type-provider-zod";
 
 // ? Routes
-import { getAllEnvironmentalData } from "./routes/get-all-environmental-data";
-import { getEnvironmentalDataByDate } from "./routes/get-environmental-data-by-date";
+import { getAllEnvironmentalMetrics } from "./routes/get-all-environmental-metrics";
+import { getEnvironmentalMetricByDate } from "./routes/get-environmental-metric-by-date";
 import { getNotifications } from "./routes/get-notifications";
 import { readNotification } from "./routes/read-notification";
 import { hasUnreadNotifications } from "./routes/has-unread-notifications";
@@ -19,8 +19,8 @@ export const app = fastify().withTypeProvider<ZodTypeProvider>();
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
-app.register(getAllEnvironmentalData);
-app.register(getEnvironmentalDataByDate);
+app.register(getAllEnvironmentalMetrics);
+app.register(getEnvironmentalMetricByDate);
 
 app.register(getNotifications);
 app.register(readNotification);
