526408ae96060489d9360f6e50b8987962498577
Guilherme Aranega <guilhermee.aranega@gmail.com>
2025-05-06 00:03:49 -0300

    feat: add HTTP route to remotely control irrigation pump using MQTT

---
+++ src/routes/control-pump.ts
@@ -0,0 +1,34 @@
+import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { turnPumpOn, turnPumpOff } from "../services/pump-controller";
+
+export async function controlPump(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    "/pumps",
+    {
+      schema: {
+        body: z.object({
+          action: z.enum(["ON", "OFF"]),
+          deviceId: z.number(),
+        }),
+        response: {
+          200: z.object({
+            message: z.string(),
+            status: z.boolean(),
+          }),
+        },
+      },
+    },
+    async (req, res) => {
+      const { action, deviceId } = req.body;
+
+      if (action === "ON") turnPumpOn(deviceId);
+      if (action === "OFF") turnPumpOff(deviceId);
+
+      return res
+        .status(200)
+        .send({ status: true, message: "Pump action executed" });
+    }
+  );
+}
